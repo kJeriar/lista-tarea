fbc07b6f37199923519e062e72052c34caa69a23
kJeriar <karla.jeria@gmail.com>
2017-04-27 12:22:04 -0300

    Update main.js
    alguna vez hice las funciones aca

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,8 @@
-function cambiarGato(){
-	var contenedor = document.getElementById("contenedor-gatos");
-	var numRandom = Math.floor(Math.random() * res.data.length);
 
-	contenedor.innerHTML = '<img src="' + res.data[numRandom].images.downsized.url + '">';
-}
-
-function masTarea(){
-
-	
-}
-
-function obtenerEmail(){
-	var input = document.getElementById("email");
-
-	var contEmail = document.getElementsByClassName("email-list");
-
-	contEmail[0].innerHTML += input.value +"<br>";
-
-	input.value = "";
-}
-
-cambiarGato();
+//function listarTareas(){
+  //var listado=document.getElementById("listarT")
+//hago un arreglo con for para imprimir las 10 primeras tareas
+//document.write('<div> <h1> Las tareas son: </h1></div>');
+//for (var i = 0; i <res[11]; i++) {
+ // listado.innerHTML += "<li>" + res[i].title + "</li>";
+  //  document.write("<div><li>" + res[i].title + "</div></li>");
